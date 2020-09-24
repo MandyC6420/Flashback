d8510324005fa54842a5c6715aaf0156e4743e26
Mandy <drgnfli6420@yahoo.com>
2020-09-24 14:19:21 -0400

    edited appsettings to correct server name

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Flashback.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public ApplicationUser()
+        {
+
+        }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        public List<Meeting> Meetings { get; set; }
+        public List<MeetingAttendant> MeetingAttendants { get; set; }
+        public List<Message> Messages { get; set; }
+    }
+}
---
+++ Models/Meeting.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Flashback.Models
+{
+    public class Meeting
+    {
+        [Key]
+        public int MeetingId { get; set; }
+        public int UserId { get; set; }
+        public DateTime StartDateTime { get; set; }
+        public DateTime EndDateTime { get; set; }
+        public string VenueChannel { get; set; }
+        public string Agenda { get; set; }
+
+        public ApplicationUser ApplicationUser { get; set; }
+
+        public List<Message> Messages { get; set; }
+
+    }
+}
