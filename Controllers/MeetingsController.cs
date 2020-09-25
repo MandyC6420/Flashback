3f5dc135ffadf403081a9385bd3fbf9ec12bd965
Mandy <drgnfli6420@yahoo.com>
2020-09-25 14:32:00 -0400

    controllers created

---
+++ Controllers/MeetingsController.cs
@@ -0,0 +1,164 @@
+﻿using Flashback.Data;
+using Flashback.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Flashback.Controllers
+{
+    public class MeetingsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public MeetingsController(ApplicationDbContext ctx,
+                          UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+            _context = ctx;
+        }
+
+        private Task<ApplicationUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
+
+        // GET: Meetings
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Meeting.Include(m => m.User);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Meetings/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var meeting = await _context.Meeting
+                .Include(m => m.User)
+                .FirstOrDefaultAsync(m => m.MeetingId == id);
+            if (meeting == null)
+            {
+                return NotFound();
+            }
+
+            return View(meeting);
+        }
+
+        // GET: Meetings/Create
+        public IActionResult Create()
+        {
+            ViewData["UserId"] = new SelectList(_context.User, "Id", "Id");
+            return View();
+        }
+
+        // POST: Meetings/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MeetingId,UserId,StartDateTime,EndDateTime,VenueChannel,Agenda")] Meeting meeting)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(meeting);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.User, "Id", "Id", meeting.UserId);
+            return View(meeting);
+        }
+
+        // GET: Meetings/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var meeting = await _context.Meeting.FindAsync(id);
+            if (meeting == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserId"] = new SelectList(_context.User, "Id", "Id", meeting.UserId);
+            return View(meeting);
+        }
+
+        // POST: Meetings/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("MeetingId,UserId,StartDateTime,EndDateTime,VenueChannel,Agenda")] Meeting meeting)
+        {
+            if (id != meeting.MeetingId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(meeting);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MeetingExists(meeting.MeetingId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.User, "Id", "Id", meeting.UserId);
+            return View(meeting);
+        }
+
+        // GET: Meetings/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var meeting = await _context.Meeting
+                .Include(m => m.User)
+                .FirstOrDefaultAsync(m => m.MeetingId == id);
+            if (meeting == null)
+            {
+                return NotFound();
+            }
+
+            return View(meeting);
+        }
+
+        // POST: Meetings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var meeting = await _context.Meeting.FindAsync(id);
+            _context.Meeting.Remove(meeting);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MeetingExists(int id)
+        {
+            return _context.Meeting.Any(e => e.MeetingId == id);
+        }
+    }
+}
